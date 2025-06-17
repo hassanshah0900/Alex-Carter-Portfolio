a34e71a2e9f086d092697986d2507a1cc808c0a0
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-17 17:52:58 +0500

    feat: create ProjectSection

---
+++ src/components/sections/ProjectsSection.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useMemo, useRef, useState } from "react";
+import Project from "../Project";
+import Button from "../Button";
+import { twMerge } from "tailwind-merge";
+
+const projects = [
+  {
+    id: 1,
+    title: "Delicious Donuts",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe rerum velitnam officiis totam laboriosam ipsa soluta perspiciatis consequatur vitae alias corporis, cupiditate voluptatem voluptas sunt. Debitis temporibus excepturi atque!",
+    imgUrl: "/donut.png",
+  },
+  {
+    id: 2,
+    title: "Naturea",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe rerum velitnam officiis totam laboriosam ipsa soluta perspiciatis consequatur vitae alias corporis, cupiditate voluptatem voluptas sunt. Debitis temporibus excepturi atque!",
+    imgUrl: "/naturea.png",
+  },
+  {
+    id: 3,
+    title: "Digital Marketing",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe rerum velitnam officiis totam laboriosam ipsa soluta perspiciatis consequatur vitae alias corporis, cupiditate voluptatem voluptas sunt. Debitis temporibus excepturi atque!",
+    imgUrl: "/digital-marketing.png",
+  },
+  {
+    id: 4,
+    title: "Donut",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe rerum velitnam officiis totam laboriosam ipsa soluta perspiciatis consequatur vitae alias corporis, cupiditate voluptatem voluptas sunt. Debitis temporibus excepturi atque!",
+    imgUrl: "/donut.png",
+  },
+  {
+    id: 5,
+    title: "Naturea",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe rerum velitnam officiis totam laboriosam ipsa soluta perspiciatis consequatur vitae alias corporis, cupiditate voluptatem voluptas sunt. Debitis temporibus excepturi atque!",
+    imgUrl: "/naturea.png",
+  },
+];
+
+export default function ProjectsSection({ className }: { className?: string }) {
+  const [isShowingAll, setShowingAll] = useState(false);
+  const ref = useRef<HTMLSelectElement>(null);
+  const filteredProjects = isShowingAll ? projects : projects.slice(0, 2);
+  return (
+    <section
+      ref={ref}
+      className={twMerge("space-y-40 relative", className)}
+      id="projects"
+    >
+      {filteredProjects.map((project) => (
+        <Project
+          key={project.id}
+          title={project.title}
+          description={project.desc}
+          imgUrl={project.imgUrl}
+          alignment={project.id % 2 === 0 ? "right" : "left"}
+        />
+      ))}
+      <Button
+        onClick={(e) => {
+          setShowingAll(!isShowingAll);
+        }}
+        className={`bottom-5 left-1/2 -translate-x-1/2 z-50 ${
+          isShowingAll ? "sticky " : "absolute"
+        }`}
+      >
+        {isShowingAll ? "See Less" : "See More"}
+      </Button>
+    </section>
+  );
+}

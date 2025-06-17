023a0bd15853a06fb5a6472b0f034ec9d40f33c0
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-17 17:49:01 +0500

    feat: create ProjectDescription component

---
+++ src/components/ProjectDescription.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  children: string;
+}
+
+export default function ProjectDescription({ children }: Props) {
+  return (
+    <p className="text-start py-4 px-6 rounded-lg backdrop-blur-lg bg-radial from-accent/15 from-0% via-blue-400/10 via-70% to-blue-400/5 to-100%">
+      {children}
+    </p>
+  );
+}

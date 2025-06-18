434d689adb6fd7f6135d551546ab24202c8a9aa7
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-18 19:30:40 +0500

    feat: make ContactSection responsive

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -8,22 +8,22 @@ import { twMerge } from "tailwind-merge";
 export default function ContactSection({ className }: { className?: string }) {
   return (
     <section className={twMerge(className)} id="contact">
-      <h2 className="text-2xl mb-10">Contact</h2>
-      <p className="mb-10">
+      <h2 className="text-xl sm:text-2xl mb-5 sm:mb-10">Contact</h2>
+      <p className="mb-10 leading-relaxed">
         I'm currently looking to join a cross-functional team that values
         improving people's lives through accessible design. or have a project in
         mind? Let's connect.
       </p>
 
       <div className="grid grid-cols-[repeat(2,max-content)] gap-5 justify-start justify-items-start">
-        <Link href="/" className="mt-20 mb-10">
+        <Link href="/" className="mt-16 sm:mt-20 mb-10">
           alexcarter@gmail.com
         </Link>
         <span className="relative text-accent">
           my Mail{" "}
           <Image
             unoptimized
-            className="absolute right-[120%]"
+            className="w-12 sm:w-auto absolute right-[120%]"
             src={arrow}
             alt=""
           />

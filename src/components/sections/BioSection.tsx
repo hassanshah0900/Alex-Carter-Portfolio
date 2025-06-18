e3436d4e8435360b397583e68b59cfbb0374f183
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-06-18 19:29:49 +0500

    feat: make BioSection responsive

--- src/components/sections/BioSection.tsx
+++ src/components/sections/BioSection.tsx
@@ -6,23 +6,12 @@ import { twMerge } from "tailwind-merge";
 
 export default function BioSection({ className }: { className?: string }) {
   return (
-    <section className={twMerge(className)} id="bio">
-      <div className="flex items-center mb-32">
-        <div className="self-end">
-          <Avatar />
-        </div>
-        <div className="flex flex-col relative">
-          <div className="text-lg mb-24 relative">
-            <Image
-              className="absolute bottom-0 right-[105%] top-1/2"
-              src={arrowVector}
-              alt=""
-            />
-            Hello! I Am{" "}
-            <h1 className="text-accent inline-block">Alex Carter</h1>
-          </div>
-          <span>A Designer who</span>
-          <span className="text-5xl leading-tight mb-2">
+    <section className={twMerge("relative pt-14", className)} id="bio">
+      <div className="flex items-end mb-18 sm:mb-32">
+        <Avatar />
+        <div className="flex flex-col items-start relative">
+          <span className="text-xs sm:text-base">A Designer who</span>
+          <span className="text-xl xs:text-2xl sm:text-5xl leading-tight mb-2">
             Judges a book <br /> by its{" "}
             <span className="text-accent relative inline-block">
               cover{" "}
@@ -33,20 +22,21 @@ export default function BioSection({ className }: { className?: string }) {
             </span>
             ...
           </span>
-          <span className="text-xs">
+          <span className="text-2xs xs:text-xs text-wrap">
             Because if the cover does not impress you what else can?
           </span>
         </div>
       </div>
 
-      <h1 className="text-5xl mb-10">I'm a Graphic Designer.</h1>
-      <p className="w-[80%] leading-loose">
+      <h1 className="text-xl xs:text-2xl sm:text-5xl mb-5 sm:mb-10">
+        I'm a Graphic Designer.
+      </h1>
+      <p className="w-full text-xs sm:text-base sm:w-[80%] leading-loose">
         Alex Carter is a Denver-based graphic designer with 6+ years of
         experience crafting strategic visual identities. He specializes in
         translating complex ideas into clean, impactful designs across branding,
         digital (UI/UX, social), and print. Alex believes design solves problems
-        and tells unforgettable stories. His style balances modern minimalism
-        with vibrant purpose, driven by typography and intuitive layout.
+        and tells unforgettable stories.
       </p>
     </section>
   );
@@ -54,14 +44,27 @@ export default function BioSection({ className }: { className?: string }) {
 
 function Avatar() {
   return (
-    <div className="relative">
-      <Gradient className="w-[150%]" color="purple" />
-      <Gradient brightness="high" className="w-full" />
-      <Image
-        className="w-40 px-10 box-content"
-        src={avatar}
-        alt="It is avatar image of Alex Carter with Apple laptop in front and avatar is facing you."
-      />
+    <div className="shrink-0">
+      <div className="absolute top-0 left-0 translate-x-34 sm:translate-x-62">
+        <Image
+          className="w-16 sm:w-auto absolute bottom-0 right-[105%] top-1/2"
+          unoptimized
+          src={arrowVector}
+          alt=""
+        />
+        <div className="text-nowrap text-sm sm:text-lg">
+          Hello! I Am <h1 className="text-accent inline-block">Alex Carter</h1>
+        </div>
+      </div>
+      <div className="relative">
+        <Gradient className="w-[150%]" color="purple" />
+        <Gradient brightness="high" className="w-full" />
+        <Image
+          className="w-32 sm:w-60 px-5 sm:px-10"
+          src={avatar}
+          alt="It is avatar image of Alex Carter with Apple laptop in front and avatar is facing you."
+        />
+      </div>
     </div>
   );
 }
